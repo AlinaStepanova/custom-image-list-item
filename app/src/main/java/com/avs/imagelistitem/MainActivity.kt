b45c4a11a1d73210925f22e3b683dfd5ef1b3e2c
AlinaStepanova <astepanova@yukon.cv.ua>
2021-04-24 19:06:43 +0300

    load image into shapeable image view

--- app/src/main/java/com/avs/imagelistitem/MainActivity.kt
+++ app/src/main/java/com/avs/imagelistitem/MainActivity.kt
@@ -2,10 +2,36 @@ package com.avs.imagelistitem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.avs.imagelistitem.databinding.ActivityMainBinding
+import com.google.android.material.shape.CornerFamily
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropTransformation
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val widthRatio = 1F
+        val heightRatio = 1F
+        val url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Nelumno_nucifera_open_flower_-_botanic_garden_adelaide2.jpg/1200px-Nelumno_nucifera_open_flower_-_botanic_garden_adelaide2.jpg"
+        binding.ivPoster.shapeAppearanceModel = binding.ivPoster.shapeAppearanceModel
+                .toBuilder()
+                .build()
+        Picasso.get()
+                .load(url)
+                .transform(
+                        CropTransformation(
+                                widthRatio,
+                                heightRatio,
+                                CropTransformation.GravityHorizontal.CENTER,
+                                CropTransformation.GravityVertical.TOP
+                        )
+                )
+                .into(binding.ivPoster)
     }
 }
